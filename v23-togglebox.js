53bbf27ed913664ddc24d64e55a311f65a3a4bc1
mvelarde238 <velarde23soluciones@gmail.com>
2022-07-29 14:38:51 -0500

    small refactorization

--- v23-togglebox.js
+++ v23-togglebox.js
@@ -100,8 +100,7 @@
 			return true;
 		},
 		_saveItems(){
-			var nav = this.el.getElementsByClassName('v23-togglebox__nav')[0];
-			var btns = nav.getElementsByClassName('v23-togglebox__btn');
+			var btns = this.nav.getElementsByClassName('v23-togglebox__btn');
 			for (var i = 0; i < btns.length; i++) {
 				var boxid = btns[i].dataset.boxid;
 
